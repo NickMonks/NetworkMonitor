def6bd2276c80e6857cb19309463ded1a37c4b3d
NickMonks <nicolasmg94@gmail.com>
2021-06-04 19:01:08 +0100

    Playground testing with boost asio library

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <boost/asio.hpp>
 #include <boost/system/error_code.hpp>
+#include <thread>
+#include <iomanip>
 
 using tcp = boost::asio::ip::tcp;
 
@@ -10,16 +12,26 @@ void Log(boost::system::error_code ec){
                 << std::endl;
 }
 
+void OnConnect(boost::system::error_code ec)
+{
+
+Log(ec);
+
+}
+
 
 
 int main() {
     
+    // 
+    std::cerr << "[" << std::setw(14) << std::this_thread::get_id() << "] main" << std::endl;
+
     // Always start with an IO Context: intermediary between the different I/O interfaces
     // and get a result back
     boost::asio::io_context ioc {};
 
     // Create an I/O object, which needs a context:
-    tcp::socket socket {ioc};
+    tcp::socket socket {boost::asio::make_strand(ioc)};
 
     // Under the hood, socket.connect uses I/O COntext to talk to the socket and get a response back. 
     // response is saved in ec
@@ -33,15 +45,37 @@ int main() {
     tcp::resolver resolver {ioc};
     auto resolverIt {resolver.resolve("google.com", "80", ec)};
 
-    
-
-    socket.connect(endpoint, ec);
-
     if (ec){
         std::cerr << "Error: " << ec.message() << std::endl;
         return -1;
-    } else {
-        std::cout << "OK" <<std::endl;
-        return 0;
-    }
+    } 
+    
+    size_t nThreads {4};
+    /* Async connection
+    * @{Nick}
+    */
+
+   for (size_t idx {0}; idx < nThreads; ++idx){
+       // For each thread we will call async_connect 4 times. This will be done using io_context::run on a separate thread
+       socket.async_connect(*resolverIt, OnConnect);
+   }
+
+   std::vector<std::thread> threads{};
+
+   threads.reserve(4);
+   for (size_t idx {0}; idx < nThreads; ++idx){
+       threads.emplace_back(
+           [&ioc](){
+            std::cerr << "[" << std::setw(14) << std::this_thread::get_id() << "] ioc.run()" << std::endl;
+            ioc.run();
+        }
+       );
+   }
+
+    // we must call io_context::run for asynchronous callbacks to run.
+    // we can run the io context in a different thread
+    std::thread thread {
+    };
+
+    thread.join();
 }
